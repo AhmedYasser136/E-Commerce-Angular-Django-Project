64fccb757087f7270baea6be46a252cac825fe50
RahmaEl-Sheikh <eng.rahma.elsheikh@gmail.com>
2023-05-16 19:08:02 +0300

    linking project

--- E-Commerce-Angular-Django-Project/src/app/auth/auth.service.ts
+++ E-Commerce-Angular-Django-Project/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,18 @@ export class AuthService {
   }
   public isUserRegistered = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   setUserRegistered() {
     this.isUserRegistered = true;
   }
 
+  login(username: string, password: string) {
+    return this.http.post('/api/login/', {username, password});
+  }
+
   registerUser(username: string, password: string) {
-    // Register the user and set the flag to true
+    return this.http.post('/api/register/', {username, password});
     this.setUserRegistered();
   }
 }
